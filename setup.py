0f7bcb864a6b1ec4cc72c9d06039bc168cf1d9a3
VinceCabs <vctcbns@gmail.com>
2019-10-28 08:32:04 +0100

    correct find_pack in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(name="kleine_fische",
       license="MIT Licence",
       description="Pretty simple Simulator for the famous 'Tempo, Kleine Fische!' game",
-      #packages=find_packages()
+      packages=find_packages()
       )
